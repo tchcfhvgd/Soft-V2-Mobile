c435dd1720082cb036e53df36b12d3111456e425
tchcfhvgd <2220687506@qq.com>
2025-05-24 22:09:20 +0800

    lol

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -42,7 +42,7 @@ class NoteSplash extends FlxSprite
 		colorSwap.hue = hueColor;
 		colorSwap.saturation = satColor;
 		colorSwap.brightness = brtColor;
-		offset.set(-10, -10);
+		offset.set(-30, -30);
 
 		var animNum:Int = FlxG.random.int(1, 2);
 		animation.play('note' + note + '-' + animNum, true);
